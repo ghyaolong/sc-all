bb0cf28ff442f8916a3524b5311687f3d6dae1f5
ghyaolong <289911401@qq.com>
2019-05-31 18:08:38 +0800

    增加断路器(异步实现方式)

--- service-ribbon/src/main/java/com/yao/sc/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/yao/sc/serviceribbon/service/HelloService.java
@@ -1,22 +1,45 @@
 package com.yao.sc.serviceribbon.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.command.AsyncResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.Future;
+
 
 @Service
 public class HelloService {
 
     @Autowired
     RestTemplate restTemplate;
-    @HystrixCommand(fallbackMethod = "hiError")
+    /**
+     * 同步实现断路器
+     */
+    /*@HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
     }
 
     public String hiError(String name){
         return "hi,"+name+",sorry,error!";
+    }*/
+
+
+    /**
+     * 异步实现断路器
+     * @param name
+     * @return
+     */
+    @HystrixCommand
+    public Future<String> hiService(String name) {
+        return new AsyncResult<String>() {
+            @Override
+            public String invoke() {
+                return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+            }
+        };
     }
 
 
