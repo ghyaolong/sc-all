463830a935c314c6897c9d0de7d1b1a2a9c562a3
yaochenglong <289911401@qq.com>
2019-06-19 23:49:50 +0800

    zuul

--- dynamic-route/src/main/java/com/yao/sc/dynamicroute/ApiGatewayDynamicRouteApplication.java
+++ dynamic-route/src/main/java/com/yao/sc/dynamicroute/ApiGatewayDynamicRouteApplication.java
@@ -3,11 +3,13 @@ package com.yao.sc.dynamicroute;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableZuulProxy
 public class ApiGatewayDynamicRouteApplication {
@@ -16,11 +18,11 @@ public class ApiGatewayDynamicRouteApplication {
         SpringApplication.run(ApiGatewayDynamicRouteApplication.class, args);
     }
 
-    @Bean
+    /*@Bean
     @RefreshScope
     @ConfigurationProperties("zuul")
     public ZuulProperties zuulProperties(){
         return new ZuulProperties();
-    }
+    }*/
 
 }
