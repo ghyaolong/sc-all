85813167de5c3a688bbc9522dc8b34924556e754
yaochenglong <289911401@qq.com>
2019-06-25 01:09:28 +0800

    增加服务链路追踪demo。 service-hi service-miya 另需要zipkin-server-2.10.1-exec.jar

--- service-hi/src/main/java/com/yao/sc/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/yao/sc/servicehi/ServiceHiApplication.java
@@ -1,12 +1,18 @@
 package com.yao.sc.servicehi;
 
+import brave.sampler.Sampler;
 import com.sc.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
@@ -14,6 +20,14 @@ public class ServiceHiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 
     @Value("${server.port}")
@@ -33,5 +47,24 @@ public class ServiceHiApplication {
     public String hello3(@RequestBody User user){
         return "Hello "+user.getUserName()+","+user.getAge();
     }
+
+    @RequestMapping("/hi1")
+    public String callHome(){
+        log.info("calling trace service-hi");
+        return restTemplate.getForObject("http://localhost:8988/miya",String.class);
+    }
+
+    @RequestMapping("/info")
+    public String info(){
+        log.info("calling trace service-hi ");
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
 
 }
---
+++ service-miya/src/main/java/com/yao/sc/servicemiya/ServiceMiyaApplication.java
@@ -0,0 +1,50 @@
+package com.yao.sc.servicemiya;
+
+import brave.sampler.Sampler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@RestController
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceMiyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMiyaApplication.class, args);
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/hi")
+    public String callHome(){
+        log.info("calling trace service-hi");
+        return restTemplate.getForObject("http://localhost:8763/miya", String.class);
+    }
+    @RequestMapping("/miya")
+    public String info(){
+        log.info("calling trace service-hi ");
+
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+}
