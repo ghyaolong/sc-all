4c3e2538950122fc796ea51a4468c78467872341
ghyaolong <289911401@qq.com>
2019-06-26 16:28:47 +0800

    add spring-boot-admin

---
+++ admin-server/src/main/java/com/yao/sc/adminserver/AdminServerApplication.java
@@ -0,0 +1,19 @@
+package com.yao.sc.adminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
